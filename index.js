0ee15421decafac0b03c6f12ade9cd8a3496b29d
satyamkushwaha <satyamkushwaha1101@gmail.com>
2022-08-08 16:52:07 +0530

    add another path for testing

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const port = process.env.PORT || 4003;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John Doe",
+    age:16
+  });
+});
+app.get("/goal", (req, res) => {
+  res.send([
+    {
+        1: "John Doe",
+        2: "Doe",
+        3: "Jane",
+        4: "Smith",
+        5: "Mike",
+    }
+  ]);
+});
 
 app.listen(port, () => {
   console.log("app is listening on port " + port);
